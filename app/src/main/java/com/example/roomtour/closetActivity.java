d2ef2b07fb0dccfcd11e5b7aa24d546ff762a5bf
loje <lojixe@gmail.com>
2019-08-04 21:02:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/roomtour/closetActivity.java
@@ -0,0 +1,44 @@
+package com.example.roomtour;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class closetActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.Jackets:
+                    mTextMessage.setText(R.string.title_Jackets);
+                    return true;
+                case R.id.clothes:
+                    mTextMessage.setText(R.string.title_clothes);
+                    return true;
+                case R.id.floor:
+                    mTextMessage.setText(R.string.title_5floor);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_closet);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
